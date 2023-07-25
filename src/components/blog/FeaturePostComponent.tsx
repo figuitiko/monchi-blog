e7a49a3db2d6757b1a771361e9bb979a24ffe30a
frank renato <frankfigao@gmail.com>
2023-07-25 17:05:17 -0600

    Fixing fixing typescript error eslint

--- src/components/blog/FeaturePostComponent.tsx
+++ src/components/blog/FeaturePostComponent.tsx
@@ -2,9 +2,8 @@ import { IPost } from '@/types'
 import Image from 'next/image'
 import BtnLinkComponent from '../share/BtnLinkComponent'
 
-
 const FeaturePostComponent = (post: IPost) => {
-  const {id, title, date, author, excerpt, imageUrl } = post
+  const { id, title, date, author, excerpt, imageUrl } = post
   return (
     <div className='w-full h-[519px] p-20 flex justify-center gap-[141px] bg-[#F4F0F8] py-8'>
       <div className='w-[624px] flex flex-col '>
@@ -15,13 +14,14 @@ const FeaturePostComponent = (post: IPost) => {
           {title}
         </h1>
         <span className='font-inter font-medium text-[14px] leading-5'>{`Por ${author} | ${date}`}</span>
-        <div className='font-inter font-normal leading-7 mt-[1rem] mb-[2rem]' dangerouslySetInnerHTML={{ __html: excerpt }} />
+        <div className='font-inter font-normal leading-7 mt-[1rem] mb-[2rem]' dangerouslySetInnerHTML={{ __html: excerpt as string }} />
         <BtnLinkComponent url={`/blog/${id}`} text='Leer más' />
       </div>
-      <picture className='w-[515px] h-[359px]' >        
+      <picture className='w-[515px] h-[359px]' >
         {
-          imageUrl ? (<Image src={imageUrl} width={515} height={359} alt={post.title} />) :
-          (<Image src='/default_placeholder.png' width={515} height={359} alt={post.title} />)
+          imageUrl ?
+            (<Image src={imageUrl} width={515} height={359} alt={post.title} />) :
+            (<Image src='/default_placeholder.png' width={515} height={359} alt={post.title} />)
         } 
       </picture>
 

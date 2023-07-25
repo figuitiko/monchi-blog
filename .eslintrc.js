a6bcd1b145815a3f7892e0f392e5ac9b6ccaeda3
frank renato <frankfigao@gmail.com>
2023-07-25 16:58:38 -0600

    Adding rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,19 @@ module.exports = {
     '@typescript-eslint'
   ],
   rules: {
+    // You have used a rule which requires parserServices to be generated
+    // from your TypeScript configuration. You can run eslint with
+    // --debug --fix to check your configuration.
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/consistent-type-imports': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-useless-constructor': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',    
     'eslint-disable-next-line react/react-in-jsx-scope': 'off',
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
